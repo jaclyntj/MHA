30083c335e52d53cce6db6e53836be98a5c775dc
jaclyntj <03081190016@student.uph.edu>
2022-10-27 21:36:33 +0700

    load css & js

--- resources/views/layouts/adminLTE.blade.php
+++ resources/views/layouts/adminLTE.blade.php
@@ -8,11 +8,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href='{{ asset("adminLTE/plugins/fontawesome-free/css/all.min.css") }}''>
+  <link rel="stylesheet" href="/adminLTE/plugins/fontawesome-free/css/all.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href='{{ asset("adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css") }}'>
+  <link rel="stylesheet" href="/adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href='{{ asset("adminLTE/dist/css/adminlte.min.css") }}'>
+  <link rel="stylesheet" href="/adminLTE/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
@@ -256,22 +256,22 @@
 
 <!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
-<script src='{{ asset("adminLTE/plugins/jquery/jquery.min.js") }}'></script>
+<script src="/adminLTE/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src='{{ asset("adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js") }}'></script>
+<script src="/adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- overlayScrollbars -->
-<script src='{{ asset("adminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js") }}'></script>
+<script src="/adminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src='{{ asset("adminLTE/dist/js/adminlte.js") }}'></script>
+<script src="/adminLTE/dist/js/adminlte.js"></script>
 
 <!-- PAGE PLUGINS -->
 <!-- jQuery Mapael -->
-<script src='{{ asset("adminLTE/plugins/jquery-mousewheel/jquery.mousewheel.js") }}'></script>
-<script src='{{ asset("adminLTE/plugins/raphael/raphael.min.js") }}'></script>
-<script src='{{ asset("adminLTE/plugins/jquery-mapael/jquery.mapael.min.js") }}'></script>
-<script src='{{ asset("adminLTE/plugins/jquery-mapael/maps/usa_states.min.js") }}'></script>
+<script src="/adminLTE/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="/adminLTE/plugins/raphael/raphael.min.js"></script>
+<script src="/adminLTE/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="/adminLTE/plugins/jquery-mapael/maps/usa_states.min.js"></script>
 <!-- ChartJS -->
-<script src='{{ asset("adminLTE/plugins/chart.js/Chart.min.js") }}'></script>
+<script src="/adminLTE/plugins/chart.js/Chart.min.js"></script>
 
 </body>
 </html>
